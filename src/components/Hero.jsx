a1ad9c713352610938cbf5fb39de8f25ed061e89
YOUCEF54 <94319560+YOUCEF54@users.noreply.github.com>
2024-04-16 16:33:05 +0100

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ export default function Hero(){
           <div className="bg-[#AE8D70] border-gray-400 shadow-[0_35px_60px_-5px_rgba(0,0,0,0.35)] text-white  p-4 rounded-2xlf bg-opacity-95 flex flex-col items-center rounded-sm">
             <h1 className="font-bold text-4xl font-roboto ">Welcom to your shopping clothes store.</h1>
             <p className="  mt-4 font-medium text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos reprehenderit aut vel voluptates, veniam quaerat necessitatibus impedit magnam, dolorem eligendi illum nihil deserunt illo dolores quas! Consequatur obcaecati, deleniti numquam facilis sit labore?</p>
-            <button className=" w-fit my-2 mt-4 px-2 py-1 rounded-md border-[1.5px]">Shop Now!</button>
+            <button className=" w-fit my-2 mt-4 px-2 py-1 font-medium border-white rounded-md border-[1.7px]">Shop Now!</button>
           </div>
         </div>
         <img
