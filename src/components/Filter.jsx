eecc9fb4d3a098411184da811a6e6ab429923a45
YOUCEF54 <94319560+YOUCEF54@users.noreply.github.com>
2024-04-16 16:11:39 +0100

    update

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,8 @@
 import { BsGenderMale } from "react-icons/bs";
 import { BsGenderFemale } from "react-icons/bs";
 import { TbMoodKidFilled } from "react-icons/tb";
+import { GiBeveledStar } from "react-icons/gi";
+import { GoSun } from "react-icons/go";
 
 
 
@@ -25,6 +27,18 @@ export default function Filter(){
                     <div>Kids</div>
                     <button className=" bg-teal-500   p-3 font-buttonght text- rounded-full">
                     <TbMoodKidFilled className=" size-8 " color="white" />
+                    </button>
+                </button>
+                <button className="p-2 m-4 pl-4 flex space-x-4 items-center border rounded-full border-[#AE8D70]">
+                    <div>Winter</div>
+                    <button className=" bg-[#1CACCB]   p-3 font-buttonght text- rounded-full">
+                    <GiBeveledStar className=" size-8 " color="white" />
+                    </button>
+                </button>
+                <button className="p-2 m-4 pl-4 flex space-x-4 items-center border rounded-full border-[#AE8D70]">
+                    <div>Summer</div>
+                    <button className=" bg-yellow-500   p-3 font-buttonght text- rounded-full">
+                    <GoSun className=" size-8 " color="white" />
                     </button>
                 </button>
                 
